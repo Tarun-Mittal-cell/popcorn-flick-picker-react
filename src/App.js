0bccebd64fd4b4de9044b40c892005708a1577ce
Tarun Mittal <tm6622@g.rit.edu>
2023-07-01 20:00:51 -0400

    added fetch func to fetch data from external API along with using useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -53,6 +53,13 @@ const average = (arr) =>
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
+
+  useEffect(function () {
+    fetch(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=interstellar
+    `)
+      .then((res) => res.json())
+      .then((data) => console.log(data.Search));
+  }, []);
 
   return (
     <>
