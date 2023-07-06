212a39666ed8a76ceb6d91e4b9bb2e2d16bb5c21
Tarun Mittal <tm6622@g.rit.edu>
2023-07-06 19:25:29 -0400

    added abort controller

--- src/App.js
+++ src/App.js
@@ -97,8 +97,11 @@ export default function App() {
           if (data.Response === "False") throw new Error("Movie not found");
 
           setMovies(data.Search);
+          setError("");
         } catch (err) {
-          setError(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
